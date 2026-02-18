4eccc9da894ba83675d12f13968a83176ca2b185
Henry McBraida <hmcbraida@gmail.com>
2026-02-18 00:04:02 +0000

    Included demo in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "debug.hpp"
 #include "fleece.hpp"
-#include <cstdio>
+#include <cassert>
 
 void test(const char* text) {
   auto res = parse(text);
@@ -9,7 +9,40 @@ void test(const char* text) {
   printf("\n");
 }
 
-int main() {
+void example() {
+  const char* test_input =
+      "{\"my_array\": [1, 2, 3], \"my_object\": {\"my_number\": 42, "
+      "\"my_string\": \"salutations planet\"}}";
+
+  auto result = dynamic_cast<FleeceObjectNode*>(parse(test_input));
+
+  auto first_child = result->children["my_array"];
+  assert(first_child->type() == FleeceNodeType::Array);
+  printf("Array has %zu children\n",
+         dynamic_cast<FleeceArrayNode*>(first_child)->children.size());
+
+  printf("As for the object:\n");
+  auto second_child = result->children["my_object"];
+  assert(second_child->type() == FleeceNodeType::Object);
+  for (const auto& [key, value] :
+       dynamic_cast<FleeceObjectNode*>(second_child)->children) {
+    printf("Key: %s; ", key.c_str());
+    switch (value->type()) {
+    case FleeceNodeType::Number:
+      printf("Value: %f\n", dynamic_cast<FleeceNumberNode*>(value)->value);
+      break;
+    case FleeceNodeType::String:
+      printf("Value: %s\n",
+             dynamic_cast<FleeceStringNode*>(value)->value.c_str());
+      break;
+    default:
+      assert(1 == 2);
+      break;
+    }
+  }
+}
+
+void tests() {
   try {
     test("[ 45, 23 ,  \"STRING\"]");
     test("\"hello world\"");
@@ -18,4 +51,11 @@ int main() {
   } catch (char const* c) {
     printf("%s\n", c);
   }
+}
+
+int main() {
+  tests();
+  example();
+
+  return 0;
 }
